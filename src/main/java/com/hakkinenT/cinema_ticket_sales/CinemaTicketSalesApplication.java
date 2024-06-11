8fa8d9b4c95e645cc23863a1d800d7f92b971e84
Tawanna <thol_16@hotmail.com>
2024-06-11 20:52:03 -0300

    Project created

---
+++ src/main/java/com/hakkinenT/cinema_ticket_sales/CinemaTicketSalesApplication.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.cinema_ticket_sales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakkinenT/cinema_ticket_sales/CinemaTicketSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.cinema_ticket_sales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaTicketSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
